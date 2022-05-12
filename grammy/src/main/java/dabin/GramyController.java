7e9129bb6f2d6f08b5395e56348b452f54d98956
김다빈 <97892310+kdb7603@users.noreply.github.com>
2022-05-12 11:51:59 +0900

    dabin
    move

---
+++ grammy/src/main/java/dabin/GramyController.java
@@ -0,0 +1,37 @@
+package dabin;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import kr.smhrd.model.GramyUserVO;
+import kr.smhrd.service.GramyUserService;
+
+@RestController
+public class GramyController {
+
+	@Autowired
+	GramyUserService service;
+	
+	@RequestMapping(value="/Join", method=RequestMethod.POST)
+	public String Join(GramyUserVO vo) {
+		System.out.println("입력받은 회원정보");
+		System.out.println(vo);
+		System.out.println("입력받은 회원정보");
+		
+		service.GramyUserJoin(vo);
+		
+		return "success";
+	}
+	
+	@RequestMapping(value="/Login", method=RequestMethod.POST)
+	public GramyUserVO GramyUserLogin(GramyUserVO vo) {
+		
+		GramyUserVO mvo = service.GramyUserLogin(vo);
+		
+		System.out.println(vo.getUser_id());
+		
+		return mvo;
+	}
+}
---
+++ grammy/src/main/java/dabin/tb_gramy.sql
@@ -0,0 +1,3 @@
+select * from tb_user
+
+insert into tb_user (user_seq, user_id, user_pw, user_phone, user_addr, user_role, user_joindate, user_name, user_gender) values (tb_user_seq.NEXTVAL,'1', '1', '1', '1', 'user', sysdate, '1', 'man')
